3e8013576111294f9f86ec1c6694885746e86019
fassabilf <fassabilf@gmail.com>
2025-04-08 16:36:31 +0700

    chore: initialize Spring Boot auth-service project

---
+++ src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/auth/HiringgoAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringgoAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiringgoAuthApplication.class, args);
+    }
+
+}
