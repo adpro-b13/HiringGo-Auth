2b80f2909f2880bdf659a660c071c74675b10fe9
fassabilf <fassabilf@gmail.com>
2025-05-25 14:43:41 +0700

    chore: add database link

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,12 +29,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-
+    implementation("org.postgresql:postgresql:42.7.3")
     // JWT (JJWT)
     implementation("io.jsonwebtoken:jjwt-api:0.12.3") // Atau versi terbaru yang stabil
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
